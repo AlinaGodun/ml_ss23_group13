710631d42c1014fb9efbea336ef150927be60733
lexihaberl <e11806899@student.tuwien.ac.at>
2023-05-23 20:29:36 +0200

    MLP first version implemented
    Co-authored-by: Alina Godun <AlinaGodun@users.noreply.github.com>
    Co-authored-by: haberger <haberger@users.noreply.github.com>

--- Ex2/MLP.py
+++ Ex2/MLP.py
@@ -2,9 +2,38 @@ from math import log
 import numpy as np
 from sklearn.utils import check_array, check_random_state, check_X_y
 from sklearn.utils.estimator_checks import check_estimator
+from sklearn.exceptions import NotFittedError
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.neural_network import MLPClassifier as skMLP
+from sklearn.model_selection import train_test_split
+from abc import ABC, abstractmethod
 
+class ActivationFunction(ABC):
+    @abstractmethod
+    def function(self, X):
+        return NotImplemented
+    
+    @abstractmethod
+    def derivative(self, X):
+        return NotImplemented
+    
+class ReluActivation(ActivationFunction):
+    def function(self, X):
+        return np.maximum(0, X)
+    
+    def derivative(self, X):
+        return (X > 0).astype(int)
+
+class OtherActivation(ActivationFunction):
+    def function(self, X):
+       pass
+    
+    def derivative(self, X):
+        # TODO implement
+        pass
+    
 def relu(X):
-    return np.maximum(0, X)
+        return np.maximum(0, X)
 
 def second_activation_func(X):
     pass
@@ -13,12 +42,12 @@ def softmax(X):
     return (np.exp(X)/np.sum(np.exp(X), axis=1)[:, None])
 
 class MLP:
-    available_activation_functions = {
-        'relu': relu,
-        'second_activation_func': second_activation_func}
+    afs = {
+        'relu': ReluActivation,
+        'second_activation_func': OtherActivation}
     
     _estimator_type = "classifier"
-    is_fitted = False
+    _is_fitted = False
  
     def __init__(self, hidden_layer_sizes=(100,), activation_function='relu', learning_rate=0.01, n_iter=100, seed=1111):
         self.check_params(activation_function, learning_rate, n_iter)
@@ -31,10 +60,9 @@ class MLP:
 
 
     def check_params(self, activation_function, learning_rate, n_iter):
-        if activation_function not in self.available_activation_functions:
+        if activation_function not in self.afs:
             raise KeyError(f'Invalid activation function provided: {self.activation_function}. ' +
-                           f'Available activation functions: {self.available_activation_functions}')
-        
+                           f'Available activation functions: {self.afs}')
         if learning_rate < 0.0:
             raise ValueError(f'Invalid learning rate provided: {self.activation_function}. ' +
                               'Learning rate must be positive.')
@@ -46,29 +74,63 @@ class MLP:
     def cross_entropy(self, y, y_pred, weights, bias):
         #TODO regularisation with weights + bias
         loss = 0
-        print(y_pred.shape)
-        print(y_pred)
-        for y_pred in y:
-            loss += -(y_pred == y)*log(y_pred)
+        for idx, class_label in zip(range(y_pred.shape[1]), self.classes_):
+            y_prob = y_pred[:, idx]
+            loss -= (class_label == y)*log(y_prob)
         return loss
 
-    def fit(self, X, y):
-        X, y = check_X_y(X,y)
-        random_state = check_random_state(self.seed)
-        self.is_fitted = True
-        self.classes_, y = np.unique(y, return_inverse=True)
-        self.features_in_ = X.shape[0]
+    def check_regression_task(self, y):
+        if np.issubdtype(y.dtype, np.floating) and np.unique(y).shape[0] > 2:
+            # The target variable is a float indicating a regression task
+            raise ValueError(f'Unknown label type: Estimator only supports classification')
 
-        activation_function = self.available_activation_functions[self.activation_function]
+
+    def fit(self, X, y):
+        if y is None:
+            raise ValueError("MLP requires y to be passed, but the target y is None")
+        X, y = check_X_y(X,y)
+        self.check_regression_task(y)
+        random_state = check_random_state(self.seed)
+        self._is_fitted = True
+        self.classes_, y = np.unique(y, return_inverse=True)
+        self.n_features_in_ = X.shape[1]
+
+        activation_function = self.afs[self.activation_function]()
         
         self.weights_, self.bias_ = self.initialize_weights(X, random_state, self.classes_.shape[0])
+        i=0
+        for iteration in range(self.n_iter):
+            for row_idx in range(X.shape[0]):
+                X_sample = X[row_idx, :].reshape(1, -1)
+                y_sample = y[row_idx]
+                activation_values, z_values = self.feed_forward(X_sample, activation_function)
+                #TODO debug gradient shit
+                self.perform_backpropagation(activation_function, activation_values, z_values, y_sample, X_sample)
+            if i%5 == 0:
+                print(i)
+            i+=1
+        return self
 
-        for row_idx in range(X.shape[0]):
-            y_pred = self.predict(X[row_idx, :].reshape(1, -1))
-            print(f'y_pred: {y_pred}')
-            #TODO use probability for cross entropy!
-            loss = self.cross_entropy(y[row_idx], y_pred, self.weights_, self.bias_)
-            print(loss)
+    def perform_backpropagation(self, activation_function, activation_values, z_values, y, X):
+        for layer_idx in range(len(activation_values)-1, -1, -1):
+            if layer_idx == len(activation_values)-1:
+                y_indicator = np.zeros((self.classes_.shape[0]))
+                y_indicator[y] = 1.0
+                delta = activation_values[-1] - y_indicator
+            else:
+                #TODO h' shit
+                # delta = <derivative> * self.weights_ * delta
+                deriv_relu = activation_function.derivative(z_values[layer_idx])
+                delta = deriv_relu * (prev_delta @ self.weights_[layer_idx + 1].T)
+
+            if layer_idx == 0:
+                gradient = X.T @ delta
+            else:
+                gradient = activation_values[layer_idx-1].T @ delta
+            self.weights_[layer_idx] -= self.learning_rate * gradient
+            self.bias_[layer_idx] -= self.learning_rate * delta.T
+            prev_delta = delta
+
     
     def initialize_weights(self, X, random_state, num_classes):
         weights = []
@@ -89,29 +151,39 @@ class MLP:
 
 
     def predict(self, X):
-        if not self.is_fitted:
-            raise AttributeError("Tried to call predict on model that was not fitted.")
+        if not self._is_fitted:
+            raise NotFittedError("Tried to call predict on model that was not fitted.")
         X = check_array(X)
-        activation_function = self.available_activation_functions[self.activation_function]
-
-        D = self.feed_forward(X, activation_function)
-        print(D)
-        return self.classes_[np.argmax(D, axis=1)]
+        if X.shape[1] != self.n_features_in_:
+            raise ValueError("Number of features from X doesn't match with number of features that were trained on")
+        activation_function = self.afs[self.activation_function]()
+        
+        activation_values, _ = self.feed_forward(X, activation_function)
+        class_probabilities = activation_values[-1]
+        return self.classes_[np.argmax(class_probabilities, axis=1)]
     
 
     def feed_forward(self, X, activation_function):
+        #TODO maybe implement dropout for regularisation?
+        activation_values = []
+        z_values = []
         for weights, bias in zip(self.weights_, self.bias_):
             z = X @ weights + bias.T
-            X = activation_function(z)
-        y = softmax(X)
-        return y
-    
+            X = activation_function.function(z)
+            z_values.append(z)
+            # check if in last layer
+            if len(activation_values) == len(self.weights_) - 1:
+                activation_values.append(softmax(z))
+            else:
+                activation_values.append(X)
+        return activation_values, z_values
 
     def get_params(self, deep=False):
         return {"hidden_layer_sizes": self.hidden_layer_sizes,
                 "activation_function": self.activation_function,
                 "learning_rate": self.learning_rate,
-                "n_iter": self.n_iter}
+                "n_iter": self.n_iter,
+                "seed": self.seed}
 
 
     def set_params(self, **parameters):
@@ -126,14 +198,22 @@ class MLP:
     
 
 if __name__ == "__main__":
-    mlp = MLP((10,))
-    X = np.random.random_sample((100, 10))
-    y = np.random.randint(0, 2, size = (100))
-    print(X.shape)
-    print(y.shape)
-    mlp.fit(X, y)
-    y_pred = mlp.predict(X)
-    print(y)
-    print(y_pred)
+    mlp = MLP((15, 10), n_iter=200, learning_rate=0.0001)
+    X_main = np.random.random_sample((10000, 10))
+    y_main = X_main.sum(axis=1).astype(int)
+    #print(y_main)
+    X_train, X_test, y_train, y_test = train_test_split(X_main, y_main)
+    mlp.fit(X_train, y_train)
+    y_pred = mlp.predict(X_test)
+    print(accuracy_score(y_test, y_pred))
+    print(confusion_matrix(y_test, y_pred))
+    
+    skmlp = skMLP((15, 10), solver='sgd', alpha=0.0001, max_iter=200)
+    skmlp.fit(X_train, y_train)
+    y_pred = skmlp.predict(X_test)
+    print(accuracy_score(y_test, y_pred))
+    print(confusion_matrix(y_test, y_pred))
+    # print(y_main)
+    # print(y_pred)
     #check_estimator(mlp)
     
